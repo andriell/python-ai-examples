a074f96d642e0e80089d62462f71123a403b8fe6
Andrey Rybalko <root.andriell@gmail.com>
2025-05-10 20:26:56 +0300

    init

--- my/colordiff.py
+++ my/colordiff.py
@@ -1,20 +1,24 @@
 from PIL import Image
 
-im = Image.open('colordiff_in.jpg') # Can be many different formats.
-pix = im.load()
-out = Image.new('RGB', (im.size[0]-1, im.size[1]-1))
-outi = out.load()
-print(im.size)  # Get the width and hight of the image for iterating over
-for y in range(1, im.size[1]):
-    for x in range(1, im.size[0]):
-        # print(pix[x,y])
-        # outi[x-1, y-1] = (pix[x, y][0], pix[x, y][1], pix[x, y][2])
-        outi[x-1, y-1] = (
-            max(abs(pix[x,y][0]-pix[x-1,y][0]), abs(pix[x,y][0]-pix[x,y-1][0]), abs(pix[x,y][0]-pix[x-1,y-1][0])),
-            max(abs(pix[x,y][1]-pix[x-1,y][1]), abs(pix[x,y][1]-pix[x,y-1][1]), abs(pix[x,y][1]-pix[x-1,y-1][1])),
-            max(abs(pix[x,y][2]-pix[x-1,y][2]), abs(pix[x,y][2]-pix[x,y-1][2]), abs(pix[x,y][2]-pix[x-1,y-1][2])),
+def doDiff(imgin, imgout):
+    im = Image.open(imgin)
+    pix = im.load()
+    out = Image.new('RGB', (im.size[0]-1, im.size[1]-1))
+    outi = out.load()
+    print(imgin, im.size)
+    for y in range(1, im.size[1]):
+        for x in range(1, im.size[0]):
+            # print(pix[x,y])
+            # outi[x-1, y-1] = (pix[x, y][0], pix[x, y][1], pix[x, y][2])
+            outi[x-1, y-1] = (
+                max(abs(pix[x,y][0]-pix[x-1,y][0]), abs(pix[x,y][0]-pix[x,y-1][0]), abs(pix[x,y][0]-pix[x-1,y-1][0])),
+                max(abs(pix[x,y][1]-pix[x-1,y][1]), abs(pix[x,y][1]-pix[x,y-1][1]), abs(pix[x,y][1]-pix[x-1,y-1][1])),
+                max(abs(pix[x,y][2]-pix[x-1,y][2]), abs(pix[x,y][2]-pix[x,y-1][2]), abs(pix[x,y][2]-pix[x-1,y-1][2])),
         )
 
-out.save('colordiff_out.jpg')  # Save the modified pixels as .png
+    out.save(imgout)
+
+doDiff("colordiff_in.jpg", "colordiff_out.jpg")
+doDiff("colordiff_in2.jpg", "colordiff_out2.jpg")
 print("End")
 exit()
