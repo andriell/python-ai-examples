3760ab4a5c9270b5df145561e18528dbefd7d518
Andrey Rybalko <root.andriell@gmail.com>
2025-05-13 15:20:46 +0300

    init

--- neural-network/lesson 7. keras_grads.py
+++ neural-network/lesson 7. keras_grads.py
@@ -5,18 +5,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
 
 c = np.array([-40, -10, 0, 8, 15, 22, 38])
 f = np.array([-40, 14, 32, 46, 59, 72, 100])
 
 model = keras.Sequential()
-model.add(Dense(units=1, input_shape=(1,), activation='linear'))
+model.add(Input(shape=(1,)))
+model.add(Dense(units=1, activation='linear'))
 model.compile(loss='mean_squared_error', optimizer=keras.optimizers.Adam(0.1))
 
 history = model.fit(c, f, epochs=500, verbose=0)
 print("Обучение завершено")
 
-print(model.predict([100]))
+print(model.predict(x=np.array([100])))
 print(model.get_weights())
 
 plt.plot(history.history['loss'])
