8d05557aa98986967a2f8493f7f7734a0e8a00c4
Andrey Rybalko <root.andriell@gmail.com>
2025-05-16 17:41:34 +0300

    init

--- neural-network/lesson 14. CNN.py
+++ neural-network/lesson 14. CNN.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import mnist         # библиотека базы выборок Mnist
 from tensorflow import keras
-from tensorflow.keras.layers import Dense, Flatten, Dropout, Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Dense, Flatten, Dropout, Conv2D, MaxPooling2D, Input
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
@@ -22,7 +22,8 @@ x_test = np.expand_dims(x_test, axis=3)
 print( x_train.shape )
 
 model = keras.Sequential([
-    Conv2D(32, (3,3), padding='same', activation='relu', input_shape=(28, 28, 1)),
+    Input(shape=(28, 28, 1)),
+    Conv2D(32, (3,3), padding='same', activation='relu'),
     MaxPooling2D((2, 2), strides=2),
     Conv2D(64, (3,3), padding='same', activation='relu'),
     MaxPooling2D((2, 2), strides=2),
@@ -31,7 +32,7 @@ model = keras.Sequential([
     Dense(10,  activation='softmax')
 ])
 
-# print(model.summary())      # вывод структуры НС в консоль
+print(model.summary())      # вывод структуры НС в консоль
 
 model.compile(optimizer='adam',
              loss='categorical_crossentropy',
@@ -40,4 +41,46 @@ model.compile(optimizer='adam',
 
 his = model.fit(x_train, y_train_cat, batch_size=32, epochs=5, validation_split=0.2)
 
+print(model.save_weights(filepath='lesson14.weights.h5'))
+keras.utils.plot_model(
+    model,
+    to_file="lesson14.model.png",
+    show_shapes=True,
+    show_dtype=True,
+    show_layer_names=True,
+    rankdir="TB",
+    expand_nested=True,
+    dpi=200,
+    show_layer_activations=True,
+    show_trainable=True,
+)
+
 model.evaluate(x_test, y_test_cat)
+
+# Распознавание всей тестовой выборки
+pred = model.predict(x_test)
+pred = np.argmax(pred, axis=1)
+
+print(pred.shape)
+
+print(pred[:20])
+print(y_test[:20])
+
+# Выделение неверных вариантов
+mask = pred == y_test
+print(mask[:10])
+
+x_false = x_test[~mask]
+y_false = x_test[~mask]
+
+print(x_false.shape)
+
+# Вывод первых 25 неверных результатов
+plt.figure(figsize=(10,5))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(x_false[i], cmap=plt.cm.binary)
+
+plt.show()
